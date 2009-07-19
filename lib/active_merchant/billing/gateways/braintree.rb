66fef1ee27632a0e499661b07b0e4e393a0f1cc8
Chip Castle <chip@chipcastle.com>
2009-07-19 12:10:16 -0500

    add_recurring method added for Braintree recurring billing using start_date and product_sku_1 options.

--- lib/active_merchant/billing/gateways/braintree.rb
+++ lib/active_merchant/billing/gateways/braintree.rb
@@ -5,7 +5,23 @@ module ActiveMerchant #:nodoc:
       def api_url 
         'https://secure.braintreepaymentgateway.com/api/transact.php'
       end
-    
+      
+      def add_recurring(money, creditcard, options = {})
+        require 'logger'
+        logger = Logger.new("/tmp/smart_ps.log")
+        
+        post = {}
+        add_invoice(post, options)
+        add_payment_source(post, creditcard,options)        
+        add_address(post, options[:billing_address]||options[:address])
+        add_address(post, options[:shipping_address],"shipping")
+        add_customer_data(post, options)
+        add_start_date(post, options)
+        add_sku(post, options)
+        logger.info "post=#{post}\nmoney=#{money}\ncreditcard=#{creditcard}\noptions=#{options.to_yaml}"
+        
+        commit('add_recurring', money, post)
+      end
       self.supported_countries = ['US']
       self.supported_cardtypes = [:visa, :master, :american_express, :discover]
       self.homepage_url = 'http://www.braintreepaymentsolutions.com'
